8f555809ac02fcd502310866213f7f871a98cf00
Paul Em <paul3m@gmail.com>
2014-01-14 03:56:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## tiny webRTC server
+
+
+This is the server to the client side script at https://github.com/paul-em/tiny-webrtc
---
+++ test/tiny-webrtc-server_test.js
@@ -0,0 +1,10 @@
+var srv = require('../lib/tiny-webrtc-server.js');
+
+exports.initTest = {
+  setUp: function (done) {
+    done();
+  },
+  'init': function (test) {
+    test.done();
+  }
+};
